1836891132cd094a57c2df927660a61c1068d2e7
JeongHyeon-01 <auddwd19@naver.com>
2025-09-02 16:48:24 +0900

    [250902]

--- Java_18_2/src/Practice03.java
+++ Java_18_2/src/Practice03.java
@@ -1,9 +1,11 @@
-public class Practice03 {
-    public static String solution(String input, String cmd){
-        StringBuffer sb = new StringBuffer(input);
-        return "hjk";
-    }
-    public static void main(String[] args) {
+import java.util.ArrayList;
 
+public class Practice03 {
+
+
+    public static void main(String[] args) {
+        System.out.println(solution("aba","L B"));
+        System.out.println(solution("abcd","P x L P y"));
+        System.out.println(solution("abc","L L L P x L B P y"));
     }
 }
